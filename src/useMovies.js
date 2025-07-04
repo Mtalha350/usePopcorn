3f854f4072f68c57c3ad56e784ea6dd38ddefc43
Muhammad Talha Aslam <mtalhaaslam1234@gmail.com>
2025-07-04 10:48:37 +0500

    Fix: changed OMDB API fetch to HTTPS

--- src/useMovies.js
+++ src/useMovies.js
@@ -18,7 +18,7 @@ export function useMovies(query, callback) {
       async function fetchMovies() {
         try {
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal },
           );
 
